7a809ab11d487c6ace16daa4dbfc1c13fa1ab5c7
Lim Young Sik <limkk90@gmail.com>
2019-01-09 15:52:31 +0900

    Update Main.java

--- work-master/src/beat/Main.java
+++ work-master/src/beat/Main.java
@@ -7,14 +7,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		new Mp3();
-		System.out.println("되라고 시발");
-		System.out.println("SI B AL ");
-		System.out.println("아 no mu hthg");
-		System.out.println("meojoong");
-		for(int i=0; i<10; i++) {
-			System.out.println("ya :" + i);
-		}
-		System.out.println("n o Si 8");
+		//작업 시작
 
 	}
 
